e4ee4d06945c17140655e81881adeb0e8face10a
euclides <euclideskinto@gmail.com>
2023-07-22 19:04:40 -0300

    add repository, request and response

--- repository/tags_repository.go
+++ repository/tags_repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import "github.com/EuclidesKinto/golang-gin-01/model"
+
+type TagsRepository interface {
+	Save(tags model.Tags)
+	Update(tags model.Tags)
+	Delete(tagsId int)
+	FindById(tagsId int) (tags model.Tags, err error)
+	FinAll() []model.Tags
+}
